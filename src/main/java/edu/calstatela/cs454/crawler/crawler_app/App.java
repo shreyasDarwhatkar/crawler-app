b4359c5f6dd1499fa3313c909aa8099445b4a31e
akshays04 <ashetty2@calstatela.edu>
2015-02-01 16:00:37 -0800

    after stage

--- src/main/java/edu/calstatela/cs454/crawler/crawler_app/App.java
+++ src/main/java/edu/calstatela/cs454/crawler/crawler_app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello" );
+        System.out.println( "Hello Shreyas" );
         System.out.println( "Hello Akshay" );
     }
 }
